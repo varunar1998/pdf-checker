3c95ba99c90a6d1089d241eed2f39fd5da08fd5a
varunar1998 <85404783+varunar1998@users.noreply.github.com>
2026-04-30 22:02:43 +0200

    Update app (1) (1).py

--- app (1) (1).py
+++ app (1) (1).py
@@ -1,9 +1,4 @@
 
-import sys
-
-# This forces the app to use the updated sqlite3 version you added to requirements.txt
-__import__('pysqlite3')
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import chromadb
 from sentence_transformers import SentenceTransformer
